4cf1487ac650758ad3a37ddcf499de39fdf7a71d
vishwsingh <khayekmusic@gmail.com>
2023-03-22 09:38:40 -0400

    Adding

---
+++ src/main/java/com/example/SpringBootDemoJenkins/SpringBootDemoJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDemoJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootDemoJenkins/SpringBootDemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootDemoJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
